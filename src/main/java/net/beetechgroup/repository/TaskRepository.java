f8df8df3f389ad15ddb6a62c15cf39a9ff8ce4fc
Gabriel Menezes <bolsista.senaiemp18@ms.senai.br>
2024-07-15 22:24:51 -0400

    adding task repository

---
+++ src/main/java/net/beetechgroup/repository/TaskRepository.java
@@ -0,0 +1,7 @@
+package net.beetechgroup.repository;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import net.beetechgroup.entity.Task;
+
+public class TaskRepository implements PanacheRepository<Task> {
+
+}
