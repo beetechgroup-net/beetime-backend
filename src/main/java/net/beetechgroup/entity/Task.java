e89b03e7441478686f8a3a58d6f25afd13a99d8d
Gabriel Menezes <bolsista.senaiemp18@ms.senai.br>
2024-08-04 12:13:39 -0400

    Atualizando as entidades

--- src/main/java/net/beetechgroup/entity/Task.java
+++ src/main/java/net/beetechgroup/entity/Task.java
@@ -2,18 +2,15 @@ package net.beetechgroup.entity;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import net.beetechgroup.resource.input.TaskInput;
-import net.beetechgroup.resource.output.TaskItemOutput;
 import net.beetechgroup.resource.output.TaskOutput;
 
 @Data
@@ -25,36 +22,16 @@ public class Task {
     private UUID id;
     private String description;
     private String category;
-    @OneToMany(mappedBy = "task",cascade = CascadeType.PERSIST, orphanRemoval = true, fetch = FetchType.EAGER)
+    @OneToMany( cascade = CascadeType.ALL)
     private List<TaskItem> taskItems;
 
-    public Task(TaskInput taskInput) {
-        this.description = taskInput.getDescription();
-        this.category = taskInput.getCategory();
-        setTaskItems(taskInput);
-    }
-
-    private void setTaskItems(TaskInput taskInput) {
-        if (Objects.nonNull(taskInput.getTaskItems())) {
-            this.taskItems = taskInput.getTaskItems().stream().map(TaskItem::new).toList();
-        }
+    public void updateFromInput(TaskInput taskInput) {
+        this.setDescription(taskInput.getDescription());
+        this.setCategory(taskInput.getCategory());
+        this.setTaskItems(taskInput.getTaskItems().stream().map(TaskItem::new).toList());
     }
 
     public TaskOutput toOutput() {
-        TaskOutput taskOutput = new TaskOutput();
-        taskOutput.setId(this.id);
-        taskOutput.setDescription(this.description);
-        taskOutput.setCategory(this.category);
-        setTaskItemsOutput(taskOutput);
-        return taskOutput;
+        return new TaskOutput(this.id, this.description, this.category, this.taskItems.stream().map(TaskItem::toOutput).toList());
     }
-
-    private void setTaskItemsOutput(TaskOutput taskOutput) {
-        if (Objects.nonNull(this.taskItems)) {
-            List<TaskItemOutput> taskItemsOutput = this.taskItems.stream()
-                    .map(TaskItem::toOutput).toList();
-            taskOutput.setTaskItems(taskItemsOutput);
-        }
-    }
-
 }
--- src/main/java/net/beetechgroup/entity/TaskItem.java
+++ src/main/java/net/beetechgroup/entity/TaskItem.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.Data;
@@ -16,24 +15,19 @@ import net.beetechgroup.resource.output.TaskItemOutput;
 @Entity
 @NoArgsConstructor
 public class TaskItem {
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private LocalDateTime startTime;
     private LocalDateTime finishTime;
-    @ManyToOne
-    private Task task;
 
     public TaskItem(TaskItemInput taskItemInput) {
         this.startTime = taskItemInput.getStartTime();
         this.finishTime = taskItemInput.getFinishTime();
     }
 
-    public TaskItemOutput toOutput(){
-        TaskItemOutput taskItemOutput = new TaskItemOutput();
-        taskItemOutput.setId(this.id);
-        taskItemOutput.setStartTime(this.startTime);
-        taskItemOutput.setFinishTime(this.finishTime);
-        return taskItemOutput;
+    public TaskItemOutput toOutput() {
+        return new TaskItemOutput(this.id, this.startTime, this.finishTime);
     }
 }
